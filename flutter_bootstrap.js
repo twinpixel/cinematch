31d2db4d209d355e059e5e691a343180adbdaa64
twinpixel <andrea.poltronieri@gmail.com>
2025-03-28 15:53:15 +0100

    e

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4074273465"
+    serviceWorkerVersion: "2027956342"
   }
 });
